c0ec8db6cce85eb1e4a4f45a36bffceb6b0dc65d
gettixvp <68188897+gettixvp@users.noreply.github.com>
2025-03-08 02:28:03 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ load_dotenv()
 # Настройки из переменных окружения
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN', '7846698102:AAFR2bhmjAkPiV-PjtnFIu_oRnzxYPP1xVo')
 ADMIN_ID = os.getenv('ADMIN_ID', '854773231')
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = '/app/uploads'
 WEBHOOK_URL = os.getenv('WEBHOOK_URL', 'https://newtg-gp7w.onrender.com')
 KUFAR_LIMIT = int(os.getenv('KUFAR_LIMIT', 7))
 PARSE_INTERVAL = int(os.getenv('PARSE_INTERVAL', 5))
